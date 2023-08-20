ed388ac2008e592458f03d420b31816d28688d78
zongliang han <zongliangh@student.unimelb.edu.au>
2023-08-20 23:37:48 +1000

    update UI, add and update multiple meanings

--- clientApp/src/main/java/client/AddPanel.java
+++ clientApp/src/main/java/client/AddPanel.java
@@ -6,9 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddPanel extends JPanel {
-    private String addMsg;
     private JTextArea textArea;
-    private JTextField wordField;
+    private JButton addBtn;
+//    private JTextField wordField;
+//    private JPanel textFieldPanel;
+    private int numMean = 1;
 
     public AddPanel() {
 
@@ -24,36 +26,67 @@ public class AddPanel extends JPanel {
         add(label, constraints);
         // Create labels and text fields
 //        JLabel wordLabel = new JLabel("search");
-
         JLabel wordLabel = new JLabel("New Word");
         constraints.gridx = 0; // Column 0
-        constraints.gridy = 2; // Row 0
+        constraints.gridy = 1; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(wordLabel, constraints);
 
         JTextField wordField = new JTextField(30);
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 2; // Row 0
+        constraints.gridy = 1; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(wordField, constraints);
 
+        JButton clearBtn = new JButton("Clear");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = 1; // Row 0
+        add(clearBtn, constraints);
+
+        clearBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clear();
+            }
+        });
+
+
+//        textFieldPanel = new JPanel();
+//        textFieldPanel.setLayout(new GridBagLayout());
+//        JScrollPane scrollPane = new JScrollPane(textFieldPanel);
+//        add(textFieldPanel);
+
+
         JLabel meanLabel = new JLabel("Meaning");
         constraints.gridx = 0; // Column 0
-        constraints.gridy = 4; // Row 0
+        constraints.gridy = 2; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(meanLabel, constraints);
 
-
         JTextField meaningField = new JTextField(30);
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 4; // Row 0
+        constraints.gridy = 2; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(meaningField, constraints);
 
-        JButton addBtn = new JButton("Add");
+        JButton plusBtn = new JButton("+");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = 2; // Row 0
+        add(plusBtn, constraints);
+
+
+        this.addBtn = new JButton("Add");
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 6; // Row 0
+        constraints.gridy = 3; // Row 0
         add(addBtn, constraints);
+
+
+        plusBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addMeanField(constraints);
+            }
+        });
 
         addBtn.addActionListener(new ActionListener() {
             @Override
@@ -70,24 +103,79 @@ public class AddPanel extends JPanel {
             }
         });
 
+
         this.textArea = new JTextArea();
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 8; // Row 0
+        constraints.gridy = 4; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(textArea, constraints);
         textArea.setVisible(false);
 
-        JButton clearBtn = new JButton("Clear");
-        constraints.gridx = 3; // Column 0
-        constraints.gridy = 2; // Row 0
-        add(clearBtn, constraints);
 
-        clearBtn.addActionListener(new ActionListener() {
+    }
+
+    private void addMeanField(GridBagConstraints constraints) {
+        numMean++;
+
+        JLabel meanLabel = new JLabel("Meaning");
+        constraints.gridx = 0; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(meanLabel, constraints);
+
+        JTextField meaning = new JTextField("");
+        constraints.gridx = 1; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(meaning, constraints);
+
+        JButton minusBtn = new JButton("-");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        add(minusBtn, constraints);
+
+        minusBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                clear();
+                deleteMeanField(constraints, e);
             }
         });
+
+        constraints.gridx = 1;
+        constraints.gridy = numMean+2;
+        GridBagLayout layout = (GridBagLayout) getLayout();
+        layout.setConstraints(addBtn, constraints);
+        System.out.println("add new meaning field");
+
+        constraints.gridx = 1; // Column 0
+        constraints.gridy = numMean+3; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(textArea, constraints);
+
+        revalidate();
+        repaint();
+    }
+
+    private void deleteMeanField(GridBagConstraints constraints, ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        Component[] components = getComponents();
+        for (int i = components.length - 1; i >= 0; i--) {
+            Component component = components[i];
+            if (component instanceof JButton) {
+                JButton btn = (JButton) component;
+                if (btn == clickedButton) {
+                    JLabel label = (JLabel) components[i-2];
+                    JTextField textField = (JTextField) components[i-1];
+                    remove(label);
+                    remove(textField);
+                    remove(btn);
+                    break;
+                }
+            }
+        }
+
+        revalidate();
+        repaint();
     }
 
     private void clear() {
--- clientApp/src/main/java/client/UpdatePanel.java
+++ clientApp/src/main/java/client/UpdatePanel.java
@@ -6,8 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UpdatePanel extends JPanel {
-    private JLabel label;
     private JTextArea textArea;
+    private JButton addBtn;
+    //    private JTextField wordField;
+//    private JPanel textFieldPanel;
+    private int numMean = 1;
+
     public UpdatePanel() {
 
         setPreferredSize(new Dimension(200, 150));
@@ -22,36 +26,67 @@ public class UpdatePanel extends JPanel {
         add(label, constraints);
         // Create labels and text fields
 //        JLabel wordLabel = new JLabel("search");
-
-        JLabel wordLabel = new JLabel("Word to Update");
+        JLabel wordLabel = new JLabel("Word to Update ");
         constraints.gridx = 0; // Column 0
-        constraints.gridy = 2; // Row 0
+        constraints.gridy = 1; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(wordLabel, constraints);
 
         JTextField wordField = new JTextField(30);
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 2; // Row 0
+        constraints.gridy = 1; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(wordField, constraints);
 
+        JButton clearBtn = new JButton("Clear");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = 1; // Row 0
+        add(clearBtn, constraints);
+
+        clearBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clear();
+            }
+        });
+
+
+//        textFieldPanel = new JPanel();
+//        textFieldPanel.setLayout(new GridBagLayout());
+//        JScrollPane scrollPane = new JScrollPane(textFieldPanel);
+//        add(textFieldPanel);
+
+
         JLabel meanLabel = new JLabel("Meaning");
         constraints.gridx = 0; // Column 0
-        constraints.gridy = 4; // Row 0
+        constraints.gridy = 2; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(meanLabel, constraints);
 
-
         JTextField meaningField = new JTextField(30);
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 4; // Row 0
+        constraints.gridy = 2; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(meaningField, constraints);
 
-        JButton addBtn = new JButton("Update");
+        JButton plusBtn = new JButton("+");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = 2; // Row 0
+        add(plusBtn, constraints);
+
+
+        this.addBtn = new JButton("Update");
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 6; // Row 0
+        constraints.gridy = 3; // Row 0
         add(addBtn, constraints);
+
+
+        plusBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addMeanField(constraints);
+            }
+        });
 
         addBtn.addActionListener(new ActionListener() {
             @Override
@@ -68,25 +103,79 @@ public class UpdatePanel extends JPanel {
             }
         });
 
+
         this.textArea = new JTextArea();
         constraints.gridx = 1; // Column 0
-        constraints.gridy = 8; // Row 0
+        constraints.gridy = 4; // Row 0
         constraints.fill = GridBagConstraints.HORIZONTAL;
         add(textArea, constraints);
         textArea.setVisible(false);
 
 
-        JButton clearBtn = new JButton("Clear");
-        constraints.gridx = 3; // Column 0
-        constraints.gridy = 2; // Row 0
-        add(clearBtn, constraints);
+    }
 
-        clearBtn.addActionListener(new ActionListener() {
+    private void addMeanField(GridBagConstraints constraints) {
+        numMean++;
+
+        JLabel meanLabel = new JLabel("Meaning");
+        constraints.gridx = 0; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(meanLabel, constraints);
+
+        JTextField meaning = new JTextField("");
+        constraints.gridx = 1; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(meaning, constraints);
+
+        JButton minusBtn = new JButton("-");
+        constraints.gridx = 3; // Column 0
+        constraints.gridy = numMean+1; // Row 0
+        add(minusBtn, constraints);
+
+        minusBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                clear();
+                deleteMeanField(constraints, e);
             }
         });
+
+        constraints.gridx = 1;
+        constraints.gridy = numMean+2;
+        GridBagLayout layout = (GridBagLayout) getLayout();
+        layout.setConstraints(addBtn, constraints);
+        System.out.println("add new meaning field");
+
+        constraints.gridx = 1; // Column 0
+        constraints.gridy = numMean+3; // Row 0
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        add(textArea, constraints);
+
+        revalidate();
+        repaint();
+    }
+
+    private void deleteMeanField(GridBagConstraints constraints, ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        Component[] components = getComponents();
+        for (int i = components.length - 1; i >= 0; i--) {
+            Component component = components[i];
+            if (component instanceof JButton) {
+                JButton btn = (JButton) component;
+                if (btn == clickedButton) {
+                    JLabel label = (JLabel) components[i-2];
+                    JTextField textField = (JTextField) components[i-1];
+                    remove(label);
+                    remove(textField);
+                    remove(btn);
+                    break;
+                }
+            }
+        }
+
+        revalidate();
+        repaint();
     }
 
     private void clear() {
